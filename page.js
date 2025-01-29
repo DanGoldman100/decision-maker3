dabed6128444dfadeb079cafdf080a543643358c
DanGoldman100 <dan.goldman50@gmail.com>
2025-01-29 13:39:46 +0200

    Create page.js

---
+++ page.js
@@ -0,0 +1,11 @@
+'use client'
+
+import DecisionMaker from '@/components/DecisionMaker'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4 bg-gray-50">
+      <DecisionMaker />
+    </main>
+  )
+}
